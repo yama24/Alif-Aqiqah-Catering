5e673e278c99cdc63bf327b2ef92b572ba4137d1
marchites <gilangaryadimahardika@gmail.com>
2020-12-03 15:29:09 +0700

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -155,7 +155,7 @@
                 responsiveClass: true,
                 responsive: {
                     0: {
-                        items: 1,
+                        items: 2,
                     },
                     768: {
                         items: 6,
