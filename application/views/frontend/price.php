d06f1cb444e3468acfab4e460513bbe3df1ffe5e
marchites <gilangaryadimahardika@gmail.com>
2020-12-10 16:26:01 +0700

    Update price

--- application/views/frontend/price.php
+++ application/views/frontend/price.php
@@ -4,7 +4,7 @@
 		<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
 		<div class="container">
 			<div class="banner_content text-center">
-				<h2>About Us</h2>
+				<h2>Harga</h2>
 				<div class="page_link">
 					<a href="<?php echo base_url() ?>assets/index.html">Home</a>
 					<a href="<?php echo base_url() ?>assets/about-us.html">About</a>
@@ -14,9 +14,78 @@
 	</div>
 </section>
 <!--================End Home Banner Area =================-->
+<!--================Price Area =================-->
+<section class="price_area p_120">
+	<div class="container">
+		<div class="main_title">
+			<h2>Choose Your Price Plan</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+		</div>
+		<div class="price_inner row">
+			<div class="col-lg-4">
+				<div class="price_item">
+					<div class="price_head">
+						<h4>Real Basic</h4>
+					</div>
+					<div class="price_body">
+						<ul class="list">
+							<li><a href="#">2.5 GB Space</a></li>
+							<li><a href="#">Secure Online Transfer</a></li>
+							<li><a href="#">Unlimited Styles</a></li>
+							<li><a href="#">Customer Service</a></li>
+						</ul>
+					</div>
+					<div class="price_footer">
+						<h3><span class="dlr">$</span> 39 <span class="month">Per <br />Month</span></h3>
+						<a class="main_btn" href="#">Get Started</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="price_item">
+					<div class="price_head">
+						<h4>Real Standard</h4>
+					</div>
+					<div class="price_body">
+						<ul class="list">
+							<li><a href="#">10 GB Space</a></li>
+							<li><a href="#">Secure Online Transfer</a></li>
+							<li><a href="#">Unlimited Styles</a></li>
+							<li><a href="#">Customer Service</a></li>
+						</ul>
+					</div>
+					<div class="price_footer">
+						<h3><span class="dlr">$</span> 69 <span class="month">Per <br />Month</span></h3>
+						<a class="main_btn" href="#">Get Started</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="price_item">
+					<div class="price_head">
+						<h4>Real Ultimate</h4>
+					</div>
+					<div class="price_body">
+						<ul class="list">
+							<li><a href="#">Unlimited Space</a></li>
+							<li><a href="#">Secure Online Transfer</a></li>
+							<li><a href="#">Unlimited Styles</a></li>
+							<li><a href="#">Customer Service</a></li>
+						</ul>
+					</div>
+					<div class="price_footer">
+						<h3><span class="dlr">$</span> 99 <span class="month">Per <br />Month</span></h3>
+						<a class="main_btn" href="#">Get Started</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Price Area =================-->
 
 <!--================Made Life Area =================-->
-<section class="made_life_area p_120">
+<!--<section class="made_life_area p_120">
 	<div class="container">
 		<div class="made_life_inner">
 			<ul class="nav nav-tabs" id="myTab" role="tablist">
@@ -105,11 +174,11 @@
 			</div>
 		</div>
 	</div>
-</section>
+</section>-->
 <!--================End Made Life Area =================-->
 
 <!--================Screen Area =================-->
-<section class="screen_area text-center p_120">
+<!--<section class="screen_area text-center p_120">
 	<div class="container">
 		<div class="main_title">
 			<h2>Dalil Aqiqah</h2>
@@ -123,11 +192,11 @@
 		</div>
 		<img class="img-fluid" src="<?php echo base_url() ?>assets/img/banner/home-left-1.png" alt="">
 	</div>
-</section>
+</section>-->
 <!--================End Screen Area =================-->
 
 <!--================Made Life Area =================-->
-<section class="made_life_area p_120">
+<!--<section class="made_life_area p_120">
 	<div class="container">
 		<div class="made_life_inner">
 			<div class="row made_life_text">
@@ -147,16 +216,16 @@
 			</div>
 		</div>
 	</div>
-</section>
+</section>-->
 <!--================End Made Life Area =================-->
 
 <!--================Impress Area =================-->
 <section class="impress_area p_120">
 	<div class="container">
 		<div class="impress_inner">
-			<h2>Got Impressed to our features?</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-			<a class="banner_btn2" href="#">Request Free Demo</a>
+			<h2>Mempunyai pertanyaan seputar Alif Aqiqah ?</h2>
+			<p>Klik tombol di bawah ini, dan Anda akan terhubung dengan kami.</p>
+			<a class="banner_btn2" href="https://wa.me/081395733034">Hubungi Kami</a>
 		</div>
 	</div>
 </section>
